59b93c37f217774a55a8d7e763a8128d370570e4
Will Frank-Gemmill <will.frank-gemmill@sustainableventures.co.uk>
2018-08-07 10:35:22 +0100

    Tweaks to some reg settings so can talk to RadioHead libs on Arduino

--- RFM69.py
+++ RFM69.py
@@ -6,7 +6,7 @@ import RPi.GPIO as GPIO
 import time
 
 class RFM69(object):
-    def __init__(self, freqBand, nodeID, networkID, isRFM69HW = False, intPin = 18, rstPin = 28, spiBus = 0, spiDevice = 0):
+    def __init__(self, freqBand, nodeID, networkID, isRFM69HW = False, intPin = 18, rstPin = 29, spiBus = 0, spiDevice = 0):
 
         self.freqBand = freqBand
         self.address = nodeID
@@ -28,7 +28,7 @@ class RFM69(object):
         self.ACK_RECEIVED = 0
         self.RSSI = 0
         self.DATA = []
-        self.sendSleepTime = 0.05
+        self.sendSleepTime = 0.01
 
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.intPin, GPIO.IN)
@@ -44,13 +44,13 @@ class RFM69(object):
         self.CONFIG = {
           0x01: [REG_OPMODE, RF_OPMODE_SEQUENCER_ON | RF_OPMODE_LISTEN_OFF | RF_OPMODE_STANDBY],
           #no shaping
-          0x02: [REG_DATAMODUL, RF_DATAMODUL_DATAMODE_PACKET | RF_DATAMODUL_MODULATIONTYPE_FSK | RF_DATAMODUL_MODULATIONSHAPING_00],
+          0x02: [REG_DATAMODUL, RF_DATAMODUL_DATAMODE_PACKET | RF_DATAMODUL_MODULATIONTYPE_FSK | RF_DATAMODUL_MODULATIONSHAPING_01],
           #default:4.8 KBPS
-          0x03: [REG_BITRATEMSB, RF_BITRATEMSB_55555],
-          0x04: [REG_BITRATELSB, RF_BITRATELSB_55555],
+          0x03: [REG_BITRATEMSB, RF_BITRATEMSB_250000],
+          0x04: [REG_BITRATELSB, RF_BITRATELSB_250000],
           #default:5khz, (FDEV + BitRate/2 <= 500Khz)
-          0x05: [REG_FDEVMSB, RF_FDEVMSB_50000],
-          0x06: [REG_FDEVLSB, RF_FDEVLSB_50000],
+          0x05: [REG_FDEVMSB, RF_FDEVMSB_250000],
+          0x06: [REG_FDEVLSB, RF_FDEVLSB_250000],
 
           0x07: [REG_FRFMSB, frfMSB[freqBand]],
           0x08: [REG_FRFMID, frfMID[freqBand]],
@@ -61,33 +61,34 @@ class RFM69(object):
           # +13dBm formula: Pout=-18+OutputPower (with PA0 or PA1**)
           # +17dBm formula: Pout=-14+OutputPower (with PA1 and PA2)**
           # +20dBm formula: Pout=-11+OutputPower (with PA1 and PA2)** and high power PA settings (section 3.3.7 in datasheet)
-          #0x11: [REG_PALEVEL, RF_PALEVEL_PA0_ON | RF_PALEVEL_PA1_OFF | RF_PALEVEL_PA2_OFF | RF_PALEVEL_OUTPUTPOWER_11111],
+          0x11: [REG_PALEVEL, RF_PALEVEL_PA0_ON | RF_PALEVEL_PA1_OFF | RF_PALEVEL_PA2_OFF | RF_PALEVEL_OUTPUTPOWER_11111],
           #over current protection (default is 95mA)
           #0x13: [REG_OCP, RF_OCP_ON | RF_OCP_TRIM_95],
 
           # RXBW defaults are { REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_24 | RF_RXBW_EXP_5} (RxBw: 10.4khz)
           #//(BitRate < 2 * RxBw)
-          0x19: [REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_16 | RF_RXBW_EXP_2],
+          0x19: [REG_RXBW, RF_RXBW_DCCFREQ_111], # [REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_16 | RF_RXBW_EXP_2],
+          0x1A: [REG_AFCBW, RF_RXBW_DCCFREQ_111],
           #for BR-19200: //* 0x19 */ { REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_24 | RF_RXBW_EXP_3 },
           #DIO0 is the only IRQ we're using
           0x25: [REG_DIOMAPPING1, RF_DIOMAPPING1_DIO0_01],
           #must be set to dBm = (-Sensitivity / 2) - default is 0xE4=228 so -114dBm
-          0x29: [REG_RSSITHRESH, 220],
-          #/* 0x2d */ { REG_PREAMBLELSB, RF_PREAMBLESIZE_LSB_VALUE } // default 3 preamble bytes 0xAAAAAA
+          0x29: [REG_RSSITHRESH, 0xFF],
+          0x2d: [REG_PREAMBLELSB, RF_PREAMBLESIZE_LSB_VALUE], # default 3 preamble bytes 0xAAAAAA
           0x2e: [REG_SYNCCONFIG, RF_SYNC_ON | RF_SYNC_FIFOFILL_AUTO | RF_SYNC_SIZE_2 | RF_SYNC_TOL_0],
           #attempt to make this compatible with sync1 byte of RFM12B lib
           0x2f: [REG_SYNCVALUE1, 0x2D],
           #NETWORK ID
           0x30: [REG_SYNCVALUE2, networkID],
-          0x37: [REG_PACKETCONFIG1, RF_PACKET1_FORMAT_VARIABLE | RF_PACKET1_DCFREE_OFF |
-                RF_PACKET1_CRC_ON | RF_PACKET1_CRCAUTOCLEAR_ON | RF_PACKET1_ADRSFILTERING_OFF],
+          0x37: [REG_PACKETCONFIG1, 0xD0], # RF_PACKET1_FORMAT_VARIABLE | RF_PACKET1_DCFREE_OFF |
+                #RF_PACKET1_CRC_ON | RF_PACKET1_CRCAUTOCLEAR_ON | RF_PACKET1_ADRSFILTERING_OFF],
           #in variable length mode: the max frame size, not used in TX
-          0x38: [REG_PAYLOADLENGTH, 66],
+          0x38: [REG_PAYLOADLENGTH, 64],
           #* 0x39 */ { REG_NODEADRS, nodeID }, //turned off because we're not using address filtering
           #TX on FIFO not empty
           0x3C: [REG_FIFOTHRESH, RF_FIFOTHRESH_TXSTART_FIFONOTEMPTY | RF_FIFOTHRESH_VALUE],
           #RXRESTARTDELAY must match transmitter PA ramp-down time (bitrate dependent)
-          0x3d: [REG_PACKETCONFIG2, RF_PACKET2_RXRESTARTDELAY_2BITS | RF_PACKET2_AUTORXRESTART_ON | RF_PACKET2_AES_OFF],
+          0x3d: [REG_PACKETCONFIG2, 2], # [REG_PACKETCONFIG2, RF_PACKET2_RXRESTARTDELAY_2BITS | RF_PACKET2_AUTORXRESTART_ON | RF_PACKET2_AES_OFF],
           #for BR-19200: //* 0x3d */ { REG_PACKETCONFIG2, RF_PACKET2_RXRESTARTDELAY_NONE | RF_PACKET2_AUTORXRESTART_ON | RF_PACKET2_AES_OFF }, //RXRESTARTDELAY must match transmitter PA ramp-down time (bitrate dependent)
           #* 0x6F */ { REG_TESTDAGC, RF_DAGC_CONTINUOUS }, // run DAGC continuously in RX mode
           # run DAGC continuously in RX mode, recommended default for AfcLowBetaOn=0
@@ -98,7 +99,7 @@ class RFM69(object):
         #initialize SPI
         self.spi = spidev.SpiDev()
         self.spi.open(self.spiBus, self.spiDevice)
-        self.spi.max_speed_hz = 4000000
+        self.spi.max_speed_hz = 500000; # was 4000000
 
         # Hard reset the RFM module
         GPIO.output(self.rstPin, GPIO.HIGH);
