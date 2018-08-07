b9908ab2a866438ef466535bfe260f93b198d8e1
Will Frank-Gemmill <will.frank-gemmill@sustainableventures.co.uk>
2018-08-07 11:27:20 +0100

    Reorganised as package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='RFM69',
+		version='0.1',
+		description='Library for communicating using RFM69',
+		url='https://github.com/Dogmans/RFM69',
+		author='Will FG',
+		author_email='',
+		license='GPL-3.0',
+		packages=['RFM69'],
+		zip_safe=False)
